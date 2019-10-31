9575fcaae94ecfc4b954a6e06551663b70c337f6
maurovialich <maurovialich@hotmail.com>
2019-10-31 13:43:08 -0300

    Deleted not used files

--- www/migrado.footer.php
+++
@@ -1,7 +0,0 @@
-    </body>
-    <footer>
-        <div class='social-media'data-toggle="tooltip" title="em construção">a</div>
-        <a href='/'><div class="home">home</div></a>
-        <div class='contact'>contato: (XX) XXXX - XXXX</div>
-    </footer>
-</html>
--- www/migrado.header.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    </head>
-    <header>
-        <h1>Atendimento ao cliente</h1>
-        <div disabled class=user-icon data-toggle="tooltip" title="em construção">user</div>
-    </header>
-    <body>
